dfc1d809168b3fa6093c0b6716d89b3b9808f874
Bruno Conte <bruno@brunoc.com.br>
2022-01-23 23:09:29 -0300

    Tinderbox integration

---
+++ el-tinderbox.el
@@ -0,0 +1,109 @@
+;;; el-tinderbox.el
+;;;   by Bruno L. Conte (concat "bruno" "@" "brunoc.com.br"), 2022
+
+
+(defun tinderbox-applescript--escape-quotes (str)
+  (with-temp-buffer
+    (insert str)
+    (goto-char (point-min))
+    (while (search-forward "\"" nil t)
+      (replace-match "\\\"" "FIXEDCASE" "LITERAL"))
+    (buffer-string)))
+
+(defun tinderbox-applescript--quoted-string (str)
+  (if str
+      (format "\"%s\"" (tinderbox-applescript--escape-quotes str))
+    "missing value"))
+
+(defun tinderbox-applescript-set-attributes (attr-list)
+  (mapconcat (lambda (x) (concat "set value of attribute \"" (car x) "\" to "
+                                 (tinderbox-applescript--quoted-string (cdr x))))
+             attr-list
+             "\n"))
+
+;;;###autoload
+(defun tinderbox-make-or-update-note (name &optional text container attributes)
+  "Make a new note.
+
+`NAME' is the note name.
+
+`TEXT' is the textual content of the note.
+
+`ATTRIBUTES' is a list of cons cells with the format (attribute . value).
+
+`CONTAINER' is a string with the name of the location (a note) where to create the new note.
+If nil, the note will be created in the currently selected note."
+  (let ((script-command (format "osascript <<EOF
+set containerName to %s
+set theName to %s
+set theText to %s
+
+tell application \"Tinderbox 8\"
+	tell front document
+		if not (exists) then error \"No Tinderbox document open.\"
+
+		if containerName is not missing value and not (exists note containerName) then
+			set newContainerNote to make new note at before first note
+			tell newContainerNote to set name to containerName
+		end if
+
+		if containerName is not missing value then
+			set theContainer to note containerName
+		else
+			set theContainer to the selected note
+		end if
+
+		tell theContainer
+			if not (exists note theName) then
+				set newNote to make new note at theContainer
+			else
+				set newNote to note theName
+			end if
+			tell newNote
+				set its text to theText
+				set value of attribute \"Name\" to theName
+				%s
+			end tell
+		end tell
+	end tell
+end tell
+EOF
+"
+                                (tinderbox-applescript--quoted-string container)
+                                (tinderbox-applescript--quoted-string name)
+                                (tinderbox-applescript--quoted-string text)
+                                (tinderbox-applescript-set-attributes attributes))))
+    (shell-command script-command "*Tinderbox output*" "*Tinderbox errors*")))
+
+;;;###autoload
+(defun tinderbox-initialize-attribute (attribute attr-type &optional default-value)
+  "Create an attribute if needed"
+  (let ((script-command (format "osascript <<EOF
+set theAttributeName to %s
+set theAttributeType to %s
+set theDefaultValue to %s
+
+tell application \"Tinderbox 8\"
+	try
+		set theDocument to front document
+		if not (exists attribute theAttributeName in theDocument) then
+			set theAttribute to make attribute in theDocument
+			set type of theAttribute to theAttributeType
+			set name of theAttribute to theAttributeName
+			if theDefaultValue is not missing value then
+				set defaultValue of theAttribute to theDefaultValue
+			end if
+		end if
+	on error error_message number error_number
+		if the error_number is not -128 then display alert message error_message as warning
+		return
+	end try
+end tell
+EOF
+"
+                                (tinderbox-applescript--quoted-string attribute)
+                                (tinderbox-applescript--quoted-string attr-type)
+                                (tinderbox-applescript--quoted-string default-value))))
+    (shell-command script-command "*Tinderbox output*" "*Tinderbox errors*")))
+
+(provide 'el-tinderbox)
---
+++ phi-tinderbox.el
@@ -0,0 +1,74 @@
+;;; phi-tinderbox.el - phi-notes integration with Tinderbox
+;;; 	by Bruno L. Conte, 2022
+
+(require 'phi-notes)
+(require 'el-tinderbox)
+
+(defgroup phi-tinderbox ()
+  "Integration with Tinderbox"
+  :group 'phi)
+
+(defcustom phi-tinderbox-zettel-attr
+  "ZettelID"
+  "Attribute for note IDs"
+  :type 'string
+  :group 'phi-tinderbox)
+
+(defcustom phi-tinderbox-default-container
+  "PHI Notes"
+  "Default container for exported notes: note name or `nil' for the currently selected note."
+  :type 'string
+  :group 'phi-tinderbox)
+
+(defcustom phi-tinderbox-default-prototype
+  "PHI Note"
+  "Default prototype name for notes."
+  :type 'string
+  :group 'phi-tinderbox)
+
+
+(defcustom phi-tinderbox-url-attr
+  "URL"
+  "Attribute for note URLs"
+  :type 'string
+  :group 'phi-tinderbox)
+
+(defcustom phi-tinderbox-url-prefix
+  "x-phi://"
+  "Prefix used to set the URL of the note for the field defined by `phi-tinderbox-url-prefix'."
+  :type 'string
+  :group 'phi-tinderbox)
+
+(defun phi-tinderbox-remove-frontmatter (str)
+  (with-temp-buffer
+    (insert str)
+    (goto-char (point-min))
+    (when (looking-at-p "---") ;; remove YAML frontmatter
+      (forward-line 1)
+      (let  ((start-pos (point-min))
+             (yaml-end-pos (search-forward-regexp "^\\(\\.\\.\\.\\|---\\)" nil t)))
+        (delete-region start-pos yaml-end-pos)))
+    (goto-char (point-min))
+    (when (search-forward-regexp (concat "^[" phi-originating-symbol
+                                         phi-parent-symbol "]") nil) ;; remove breadcrumb
+          (move-end-of-line nil)
+          (delete-region (point-min) (point)))
+    (buffer-string)))
+
+;;;###autoload
+(defun phi-tinderbox-export-current-note (&optional container extra-attr)
+  "Export the current note to Tinderbox"
+  (interactive)
+  (let ((attributes (list
+                     (cons phi-tinderbox-zettel-attr (phi-get-current-note-id))
+                     (cons phi-tinderbox-url-attr (concat phi-tinderbox-url-prefix (phi-get-current-note-id)))
+                     (cons "ReadOnly" "true")
+                     (cons "Prototype" phi-tinderbox-default-prototype)))
+        (name (phi-get-current-note-title))
+        (text (phi-tinderbox-remove-frontmatter (substring-no-properties (buffer-string)))))
+    ;; (when phi-tinderbox-default-prototype
+    ;; (setq attributes (append attributes '(("Prototype" . phi-tinderbox-default-prototype)))))
+    (tinderbox-initialize-attribute phi-tinderbox-zettel-attr "String")
+    (tinderbox-make-or-update-note name text (or container phi-tinderbox-default-container)
+                                   (append attributes extra-attr))))
+
