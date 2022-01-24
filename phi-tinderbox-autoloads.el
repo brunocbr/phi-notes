ecb8cf5e743d2e5e20979b575dc32775c2e488e9
Bruno Conte <bruno@brunoc.com.br>
2022-01-24 12:04:25 -0300

    Create phi-tinderbox-autoloads.el

---
+++ phi-tinderbox-autoloads.el
@@ -0,0 +1,26 @@
+;;; phi-tinderbox-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "phi-tinderbox" "phi-tinderbox.el" (25070 2212
+;;;;;;  538919 588000))
+;;; Generated autoloads from phi-tinderbox.el
+
+(autoload 'phi-tinderbox-export-current-note "phi-tinderbox" "\
+Export the current note to Tinderbox
+
+\(fn &optional CONTAINER EXTRA-ATTR)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "phi-tinderbox" '("phi-tinderbox-")))
+
+;;;***
+
+(provide 'phi-tinderbox-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; phi-tinderbox-autoloads.el ends here
