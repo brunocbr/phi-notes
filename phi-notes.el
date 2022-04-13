05029e2a6c4253c6fa1fddffcf5435647f578634
Bruno Conte <bruno@brunoc.com.br>
2022-04-12 21:28:20 -0300

    fix citekey and loc fields in note creation

--- phi-notes.el
+++ phi-notes.el
@@ -549,9 +549,10 @@ If USECONTEXT is not nil, enforce setting the current directory to the note's di
         (w nil))
     (unless parent (setq parent (phi-get-current-note-id)))
     (let ((buffer (phi-create-common-note :id id :title title :parent parent :tags tags
-                                         (unless (string= citekey "") :citekey citekey)
-                                         (unless (string= loc "") :loc loc)
-                                         :body body (when (cadr tlg-fields) :tlg-fields tlg-fields))))
+                                          :citekey (unless (string= citekey "") citekey)
+                                          :loc (unless (string= loc "") loc)
+                                          :body body
+                                          :tlg-fields (when (cadr tlg-fields) tlg-fields))))
       (unless (string-equal "" parent)
         (insert (concat (when insert-title (concat title " "))
                         phi-link-left-bracket-symbol id phi-link-right-bracket-symbol)))
