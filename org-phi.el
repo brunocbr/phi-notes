772dc78cfa3f4624707e97e0313b1af595d2a985
brunoc <conteb@gmail.com>
2022-11-13 23:18:13 -0300

    insert bib item action is functional

--- org-phi.el
+++ org-phi.el
@@ -2,25 +2,42 @@
 
 (require 'org)
 (require 'bibtex-completion)
+(require 'ol-bibtex)
 (require 's)
 
 (defvar org-phi-bib-entry-template
   "${author}. (${year}) ${title}.
 :PROPERTIES:
-:CUSTOM_ID:	${=key=}
-:TITLE:			${title}
-:AUTHOR:		${author}
-:YEAR:			${year}
-:END:
-")
+:END:")
 
 (defun phi-bibtex-org-insert-bib-action (keys)
   (cl-loop for key in keys
            do
            (let* ((entry (bibtex-completion-get-entry key))
+	                (val (lambda (field) (cdr (assoc field entry))))
                   (heading (s-format org-phi-bib-entry-template
-                                     (lambda (x) (cdr (assoc x entry))))))
+                                     val))
+                  (org-special-properties nil) ; avoids errors with `org-entry-put'
+	                (togtag (lambda (tag) (org-toggle-tag tag 'on))))
              (org-insert-heading)
-             (insert heading))))
+             (insert heading)
+             (org-bibtex-put org-bibtex-type-property-name
+		                         (downcase (funcall val "=type=")))
+             (dolist (pair entry)
+               (pcase (car pair)
+	               ("=key=" 		(org-bibtex-put
+	                             org-bibtex-key-property (cdr pair)))
+	               ("=type=" 		nil)
+                 ("title" 		nil)
+	               ("keywords" 	(if org-bibtex-tags-are-keywords
+		                              (dolist (kw (split-string (cdr pair) ", *"))
+			                              (funcall
+			                               togtag
+			                               (replace-regexp-in-string
+			                                "[^[:alnum:]_@#%]" ""
+			                                (replace-regexp-in-string "[ \t]+" "_" kw))))
+		                            (org-bibtex-put (car pair) (cdr pair))))
+	               (_ (org-bibtex-put (car pair) (cdr pair)))))
+             (mapc togtag org-bibtex-tags))))
 
 (provide 'org-phi)
