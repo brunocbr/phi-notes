42f279718a29ede8eff5b7e9caa33d021e02db00
brunoc <conteb@gmail.com>
2022-11-19 22:03:41 -0300

    wrap lambdas inside function

--- ol-phi.el
+++ ol-phi.el
@@ -6,28 +6,31 @@
 (defun org-link-phi-open-function (name)
   "Return the open function for an Org link type corresponding to
 repository NAME."
-  (lambda (id _)
-     (phi-find-note id name)))
+  (function
+   (lambda (id _)
+     (phi-find-note id name))))
 
 (defun org-link-phi-store-function (name)
   "Create the link store function for an Org link type
 corresponding to repository NAME."
-  (lambda ()
-    (when (and (phi-mode)
-               (phi-in-repository-p (buffer-file-name (current-buffer))
-                                    name))
-      (let* ((id (phi-get-current-note-id))
-             (link (format "%s:%s" name id))
-             (title (phi-get-current-note-title)))
-        (message (format "store fn for repository %s" name))
-        (org-link-store-props
-         :type name
-         :link link
-         :description title)))))
+  (function
+   (lambda ()
+     (when (and (phi-mode)
+                (phi-in-repository-p (buffer-file-name (current-buffer))
+                                     name))
+       (let* ((id (phi-get-current-note-id))
+              (link (format "%s:%s" name id))
+              (title (phi-get-current-note-title)))
+         (message (format "store fn for repository %s" name))
+         (org-link-store-props
+          :type name
+          :link link
+          :description title))))))
 
 (defun org-link-phi-export (link description _format _channel)
   "Export wiki link for LINK"
-  (concat (when description (concat description " ")) "[[" link "]]"))
+  (lambda
+    (concat (when description (concat description " ")) "[[" link "]]")))
 
 (defun org-link-phi-register-link-types ()
   "Add Org link types for each note repository."
